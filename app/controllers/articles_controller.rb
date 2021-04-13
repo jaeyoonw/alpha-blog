f2fff203af1e79441c1583116af23e2eaef12e13
jaeyoonw <jaeyoon.dev@gmail.com>
2021-04-14 00:02:43 +0900

    make create form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,17 @@
 class ArticlesController < ApplicationController
   def show 
-    byebug  # 웹서버를 잠시 일시중지시키고 디버그 하는 라이브러리 파일 실행 
+    # byebug 웹서버를 잠시 일시중지시키고 디버그 하는 라이브러리 파일 실행 
     @article = Article.find(params[:id])
+  end 
+
+  def index 
+    @articles = Article.all 
+  end 
+
+  def new 
+  end 
+
+  def create 
+    render plain: params[:article]
   end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home' # default root URL 
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]   # articles는 테이블명, articles/show 라우트만 남기고 다룬 라우트는 제거하라는 의미 
+  resources :articles, only: [:show, :index, :new, :create]   # articles는 테이블명, articles/show 라우트만 남기고 다룬 라우트는 제거하라는 의미 
 end
