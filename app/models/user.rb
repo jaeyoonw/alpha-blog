ca2f991bcd200afa8e35c8f3e754da159f164066
jaeyoonw <jaeyoon.dev@gmail.com>
2021-06-01 17:06:45 +0900

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase } # 데이터베이스에 저장하기전에, 객체의 상태를 변경한다. 
   has_many :articles
   validates :username, presence: true, 
                        uniqueness: { case_sensitive: false }, 
@@ -8,4 +9,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }, 
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
+  has_secure_password                    
 end  
