b44e779404b9ec740ddfd97647ae305d99570222
jaeyoonw <jaeyoon.dev@gmail.com>
2021-06-11 17:14:50 +0900

    make users-signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'pages#home' # default root URL 
   get 'about', to: 'pages#about'
   resources :articles
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
