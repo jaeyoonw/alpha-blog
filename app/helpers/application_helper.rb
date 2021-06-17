bd7d1c8b13ce702a9d41734efba9a586540f53e7
jaeyoonw <jaeyoon.dev@gmail.com>
2021-06-17 20:45:46 +0900

    Cleanup layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def gravatar_for(user, options = { size: 80 })
-    email_address = user.email.downcase
+    email_address = user.email.downcase   # Alpha Blog에 등록한 email이 gravatar에 등록되잇으면 gravatar의 이미지를 보여준다. 
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
