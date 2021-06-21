16cbda7c491900a4fe2127a0927a61a27e5c1882
jaeyoonw <jaeyoon.dev@gmail.com>
2021-06-21 14:56:30 +0900

    Add login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,13 @@ class UsersController < ApplicationController
     @user = User.new
   end 
 
-  def create 
+  def create # signup get요청을 처리해주는 곳 
     @user = User.new(user_params)
     if @user.save 
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username} you have successfully signed up "
       redirect_to articles_path
     else 
-      render 'new'
+      render 'new'  # 다시 회원가입 창으로
     end 
   end
 
